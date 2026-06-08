07a4f9edb430c093c9d09b52d9b184dbc3c13c49
andrehherreira15 <andre.herreira.oliveira@escola.pr.gov.br>
2026-06-08 12:39:23 -0300

    arrays 1

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 // run `node index.js` in the terminal
 
 console.log(`Hello Node.js v${process.versions.node}!`);
-m,ljhu
+
+// //1
+// const aluno = ['Carlos', 16, '2°B']
+
+// console.log(aluno)
+
+// //2
+
+// const produto = ['Televisão', 3200, 72]
+
+// console.log(`Produto: ${produto[0]}`)
+// console.log(`Preço: ${produto[1]}`)
+// console.log(`Estoque: ${produto[2]} unidades`)
+
+//3
+
+const filmes = [
+    
+]
